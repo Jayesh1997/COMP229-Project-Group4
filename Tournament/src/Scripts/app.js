0ee11bbe44bb853b437664a1d591e7a6f87bce3f
Reihaneh Namdari <r3ihan3h@yahoo.com>
2021-11-18 17:46:42 -0500

    database setup in app.js

--- Tournament/src/Scripts/app.js
+++ Tournament/src/Scripts/app.js
@@ -5,3 +5,18 @@
     }
     window.addEventListener('load', Start);
 })();
+
+
+    //database setup
+
+let mongoose = require('mongoose');
+let DB = require('./db');
+
+// Pointing Mongoose to DB URI
+mongoose.connect(process.env.URI || DB.URI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+let mongoDB = mongoose.connection;
+mongoDB.on('error', console.error.bind(console, 'Connection Error:'));
+mongoDB.once('open', ()=> {
+  console.log("Connected to MongoDB...");
+});
