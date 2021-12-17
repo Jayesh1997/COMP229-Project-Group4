1e48a2f0d134eb7c139fa57f0043e696b0cdbc85
Jayesh Amodkar <jayeshdamodkar@gmail.com>
2021-12-17 13:54:17 -0500

    server

--- Tournament/server.js
+++ Tournament/server.js
@@ -5,28 +5,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-// app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/src'));
+app.set('view engine', 'html');
+app.set('views', 'src')
+app.get('/*', function(req,res) {
 
-// app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+'/src/index.html'));
+});
 
-// res.sendFile(path.join(__dirname+'/src/index.html'));
-// });
-let template = readFileSync(join(__dirname, '..', 'src', 'index.html')).toString();
-
-    app.engine('html', (_, options, callback) => {
-      const opts = { document: template, url: options.req.url };
-
-      renderModuleFactory(AppServerModuleNgFactory, opts)
-        .then(html => callback(null, html));
-    });
-
-    app.set('view engine', 'html');
-    app.set('views', 'src')
-
-    app.get('*.*', express.static(join(__dirname, '..', 'dist')));
-
-    app.get('*', (req, res) => {
-      res.render('index', { req });
-    });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080)
