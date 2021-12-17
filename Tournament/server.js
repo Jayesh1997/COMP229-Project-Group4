2e258e9a1c8610b9d5faa4ad4451c995fbe765a1
Jayesh Amodkar <jayeshdamodkar@gmail.com>
2021-12-17 13:49:37 -0500

    server

--- Tournament/server.js
+++ Tournament/server.js
@@ -1,107 +1,32 @@
 //Install express server
-// const express = require('express');
-// const path = require('path');
+const express = require('express');
+const path = require('path');
 
-// const app = express();
+const app = express();
 
-// // Serve only the static files form the dist directory
+// Serve only the static files form the dist directory
 // app.use(express.static(__dirname + '/src'));
 
 // app.get('/*', function(req,res) {
 
 // res.sendFile(path.join(__dirname+'/src/index.html'));
 // });
+let template = readFileSync(join(__dirname, '..', 'src', 'index.html')).toString();
 
-// // Start the app by listening on the default Heroku port
-// app.listen(process.env.PORT || 8080)
+    app.engine('html', (_, options, callback) => {
+      const opts = { document: template, url: options.req.url };
 
+      renderModuleFactory(AppServerModuleNgFactory, opts)
+        .then(html => callback(null, html));
+    });
 
+    app.set('view engine', 'html');
+    app.set('views', 'src')
 
-/**
- * Module dependencies.
- */
+    app.get('*.*', express.static(join(__dirname, '..', 'dist')));
 
-var app = require('./server/config/app');
-var debug = require('debug')('week-4:server');
-var http = require('http');
-
-/**
- * Get port from environment and store in Express.
- */
-
-var port = normalizePort(process.env.PORT || '3500');
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-
-var server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error) {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-}
+    app.get('*', (req, res) => {
+      res.render('index', { req });
+    });
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080)
